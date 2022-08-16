1a273934a19a7251e350c17fa2c5a5379fc49d23
amirography <amirhossein.alesheikh@gmail.com>
2022-08-16 12:42:05 +0430

    wip(): fix shit

--- scale.go
+++ scale.go
@@ -20,14 +20,14 @@ func (s Scale) Printer() string {
 	var attr []string
 	attr = append(attr, fmt.Sprintf(":flipped %t ", s.Flipped))
 	attr = append(attr, fmt.Sprintf(":orientation '%s' ", s.Orientation))
-	// attr = append(attr, fmt.Sprintf(":value %f ", s.Value))
-	attr = append(attr, fmt.Sprintf(":marks '%s' ", s.Marks))
-	// attr = append(attr, fmt.Sprintf(":draw-value %t ", s.DrawValue))
-	// attr = append(attr, fmt.Sprintf(":min %f ", s.Min))
-	// attr = append(attr, fmt.Sprintf(":max %f ", s.Max))
-	// attr = append(attr, fmt.Sprintf(":timeout %s ", s.Timeout))
-	// attr = append(attr, fmt.Sprintf(":onchange '%s' ", s.Onchange))
-	// attr = append(attr, s.Widget.Printer()...)
+	attr = append(attr, fmt.Sprintf(":value %f ", s.Value))
+	// attr = append(attr, fmt.Sprintf(":marks '%s' ", s.Marks)) //unstable
+	attr = append(attr, fmt.Sprintf(":draw-value %t ", s.DrawValue))
+	attr = append(attr, fmt.Sprintf(":min %f ", s.Min))
+	attr = append(attr, fmt.Sprintf(":max %f ", s.Max))
+	attr = append(attr, fmt.Sprintf(":timeout %s ", s.Timeout))
+	attr = append(attr, fmt.Sprintf(":onchange '%s' ", s.Onchange))
+	attr = append(attr, s.Widget.Printer()...)
 
 	str := stringBuilder(attr)
 

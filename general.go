5c430e4ce8efe3b663afafa32a8df306a20e70b8
amirography <amirhossein.alesheikh@gmail.com>
2022-08-16 09:29:08 +0430

    fix(): remove height

--- general.go
+++ general.go
@@ -23,7 +23,7 @@ func (w Widget) Printer() []string {
 	ws = append(ws, fmt.Sprintf(":valign '%s'", w.Valign))
 	ws = append(ws, fmt.Sprintf(":halign '%s'", w.Halign))
 	ws = append(ws, fmt.Sprintf(":width %d", w.Width))
-	ws = append(ws, fmt.Sprintf(":heigth %d", w.Heigth))
+	// ws = append(ws, fmt.Sprintf(":heigth %d", w.Heigth))
 	ws = append(ws, fmt.Sprintf(":active %t", w.Active))
 	ws = append(ws, fmt.Sprintf(":tooltip '%s'", w.Tooltip))
 	ws = append(ws, fmt.Sprintf(":visible %t", w.Visible))

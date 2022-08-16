a0efdb33843b4619f3ff57cd22b02264b2a5ffbc
amirography <amirhossein.alesheikh@gmail.com>
2022-08-16 12:43:53 +0430

    wip(): fix shit

--- scale.go
+++ scale.go
@@ -25,8 +25,8 @@ func (s Scale) Printer() string {
 	attr = append(attr, fmt.Sprintf(":draw-value %t ", s.DrawValue))
 	attr = append(attr, fmt.Sprintf(":min %f ", s.Min))
 	attr = append(attr, fmt.Sprintf(":max %f ", s.Max))
-	attr = append(attr, fmt.Sprintf(":timeout %s ", s.Timeout))
-	// attr = append(attr, fmt.Sprintf(":onchange '%s' ", s.Onchange))
+	// attr = append(attr, fmt.Sprintf(":timeout %s ", s.Timeout))
+	attr = append(attr, fmt.Sprintf(":onchange '%s' ", s.Onchange))
 	attr = append(attr, s.Widget.Printer()...)
 
 	str := stringBuilder(attr)

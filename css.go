f5bc6dc25ac161ce616e3cbc4f6a677cb86e6d88
amirography <amirhossein.alesheikh@gmail.com>
2022-08-24 10:45:29 +0430

    Revert "fix(): adjustments"
    This reverts commit eb531e5fa509cb71bf949dad133dae7e3c13ba41.

--- css.go
+++ css.go
@@ -10,12 +10,11 @@ func NewStyle() *Css {
 }
 
 func (m *Css) String() string {
-	s := " "
+	s := ""
 
 	for k, v := range *m {
 		l := fmt.Sprintf("%s: %s;", k, v)
 		s += l
-		s += " "
 	}
 
 	return s
